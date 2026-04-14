fd954e2d887bfd94fb5fb52691ad64334ef3893b
Thiago Moro do Carmo <thiago.moro77@gmail.com>
2026-04-14 13:19:17 -0300

    PostList returning container element

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,7 +1,7 @@
 import { PostInfo } from '../PostInfo';
 
 export const PostList = ({ posts }) => {
-  return posts.map(post => {
-    return <PostInfo post={post} key={post.id} />;
-  });
+  return posts.map(post => (
+    <PostInfo post={post} key={post.id} />
+  ));
 };
